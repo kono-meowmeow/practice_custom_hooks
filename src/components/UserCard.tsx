7867d9cacee5584fc12bf596a962da8972e25aac
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-11-05 21:49:18 +0900

    仮データで作成

---
+++ src/components/UserCard.tsx
@@ -0,0 +1,24 @@
+import { FC } from "react";
+import { UserProfile } from "../types/userProfile";
+
+type Props = {
+  // typesフォルダ配下で定めたtypeをimportして使う
+  user: UserProfile;
+};
+
+export const UserCard: FC<Props> = (props) => {
+  const { user } = props;
+
+  return (
+    <div>
+      <dl>
+        <dt>名前</dt>
+        <dd>{user.name}</dd>
+        <dt>メール</dt>
+        <dd>{user.email}</dd>
+        <dt>住所</dt>
+        <dd>{user.address}</dd>
+      </dl>
+    </div>
+  );
+};
