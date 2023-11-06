a908eebb7e4d3634e3322b797da13d393ecffa68
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-11-06 23:30:24 +0900

    apiで取得するUser情報の型指定

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import axios from 'axios';
 import React from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
+import { User } from './types/api/user';
 
 const user = {
   id: 1,
@@ -10,8 +12,13 @@ const user = {
 };
 
 function App() {
+  const onClickFetchUser = () => {
+    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+  }
+
   return (
     <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
       <UserCard user={user} />
     </div>
   );
