9e919788b0eaee718ee5cd6dd5a92677c5a4573b
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-11-06 23:41:34 +0900

    apiからデータを取得できるようにした

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { UserCard } from './components/UserCard';
 import { User } from './types/api/user';
+import { UserProfile } from './types/userProfile';
 
 const user = {
   id: 1,
@@ -12,13 +13,30 @@ const user = {
 };
 
 function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
   const onClickFetchUser = () => {
-    axios.get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        // mapで処理して必要なデータのみを取得する
+        // テンプレート文字列を使って、使いたい形にする
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data)
+      });
   }
 
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
       <UserCard user={user} />
     </div>
   );
