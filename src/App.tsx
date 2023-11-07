8a4ce435e613f5d148f6be62db7366dcc84cca5b
kono-meowmeow <aya.ayatsuki@gmail.com>
2023-11-07 21:42:34 +0900

    apiからデータを取得し、表示する。 ロード中や取得の失敗も伝える

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,25 @@ import { User } from './types/api/user';
 import { UserProfile } from './types/userProfile';
 
 function App() {
-  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const sampleUser: UserProfile = {
+    id: 0,
+    name: "紫宮るな",
+    email: "shinomiya@runa.com",
+    address: "https://vspo.jp/member/runa-shinomiya"
+  }
+
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([sampleUser]);
+  // ローディング中であることをユーザーに伝える
+  const [loading, setLoading] = useState<boolean>(false);
+  // ローディングの成功・失敗をユーザーに伝える
+  const [error, setError] = useState<boolean>(false);
 
   const onClickFetchUser = () => {
+    // ローディング中にする
+    setLoading(true);
+    // エラーがない状態にする
+    setError(false);
+
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -21,15 +37,32 @@ function App() {
           address: `${user.address.city}${user.address.suite}${user.address.street}`,
         }));
         setUserProfiles(data)
+      })
+      .catch(() => {
+        // エラーがあれば、エラーフラグをtrueにする
+        setError(true);
+      })
+      // thenでもcatchでもfinallyは最後に実行される
+      .finally(() => {
+        setLoading(false);
       });
   }
 
   return (
     <div className="App">
       <button onClick={onClickFetchUser}>データ取得</button>
-      {userProfiles.map((user) => (
-        <UserCard key={user.id} user={user} />
-      ))}
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
